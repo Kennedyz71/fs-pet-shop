3552964c670b76f26fb178e0696d69c5de9cf010
Kennedy <kennedyz71@gmail.com>
2023-04-06 13:25:25 -0700

    restful api

--- httpServer.js
+++ httpServer.js
@@ -1,132 +1,132 @@
-// const express = require("express");
-// const app = express();
-// const port = process.env.PORT || 8000;
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8000;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+var fs = require("fs");
+const http = require("http");
+var path = require("path");
+var petsPath = path.join(__dirname, "pets.json");
+
+app.get("/", (req, res) => {
+  res.send("what are you looking for?");
+});
+
+app.get("/pets", (req, res) => {
+  fs.readFile(petsPath, "utf8", function (err, petsJSON) {
+    let pets = JSON.parse(petsJSON);
+    if (err) {
+      console.error(err.stack);
+      res.statusCode = 500;
+      res.setHeader("Content-Type", "text/plain");
+      return res.end("Internal Server Error");
+    }
+    // res.writeHead(200, { "Content-Type": "application/json" });
+    res.send(pets);
+    //   res.setHeader("Content-Type", "application/json");
+    res.end();
+  });
+});
+
+app.get("/pets/:petIndex", (req, res) => {
+  fs.readFile(petsPath, "utf8", function (err, petsJSON) {
+    let pets = JSON.parse(petsJSON);
+    let index = pets[req.params.petIndex];
+
+    if (err) {
+      // console.error(err.stack);
+      res.statusCode = 500;
+      res.end();
+    } else if (!index) {
+      res.statusCode = 404;
+      res.end("Not found");
+    } else {
+      res.send(index);
+      res.status(200);
+      res.end();
+    }
+  });
+});
+
+app.post("/pets", (req, res) => {
+  console.log(req.body);
+  console.log(req.body.age);
+  let age = req.body.age;
+  let kind = req.body.kind;
+  let name = req.body.name;
+  console.log(age);
+  console.log(kind);
+  console.log(name);
+  // const { age, kind, name } = req.body;
+  // console.log(age, kind, name);
+  fs.readFile(petsPath, "utf8", (error, petsJSON) => {
+    if (error) {
+      res.statusCode = 500;
+      res.end();
+    }
+    // let age = JSON.stringify(JSON.parse(req.body)).match("age");
+    // let kind = JSON.stringify(JSON.parse(req.body)).match("kind");
+    // let name = JSON.stringify(JSON.parse(req.body)).match("name");
+
+    fs.readFile(petsPath, "utf8", (error, petsJSON) => {
+      let pets = JSON.parse(petsJSON);
+      let addPets = { age: age, kind: kind, name: name };
+      pets.push(addPets);
+      fs.writeFile(petsPath, JSON.stringify(pets), () => {
+        console.log(addPets);
+        res.end(petsJSON);
+      });
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`server listening on port: ${port}`);
+});
+
+// "use strict";
+
 // var fs = require("fs");
-// const http = require("http");
 // var path = require("path");
 // var petsPath = path.join(__dirname, "pets.json");
+// const express = require("express");
 
-// app.get("/", (req, res) => {
-//   res.send("what are you looking for?");
+// var http = require("http");
+// var port = process.env.PORT || 8000;
+
+//
+//   } else {
+//     res.statusCode = 404;
+//     res.setHeader("Content-Type", "text/plain");
+//     res.end("Not found");
+//   }
+//   //   if (req.method === "POST" && req.url === "/pets") {
+//   //     console.log("post");
+//   //     if (err) {
+//   //       console.error(err.stack);
+//   //       res.statusCode = 500;
+//   //       res.setHeader("Content-Type", "text/plain");
+//   //       return res.end("Internal Server Error");
+//   //     }else {
+//   //         let age = process.argv[3];
+//   //         let kind = process.argv[4];
+//   //         let name = process.argv[5];
+//   //         fs.readFile("pets.json", "utf8", (error, data) => {
+//   //          let pets = JSON.parse(data);
+//   //          let addPets = { age: age, kind: kind, name: name };
+//   //         pets.push(addPets);
+//   //         res.writeHead(200, { "Content-Type": "application/json" });
+//   //           fs.writeFile("pets.json", JSON.stringify(pets), () => {
+//   //             console.log(addPets);
+//   //          res.end(petsJSON);
+//   //          })
+//   //         });
+//   //      });
+//   //     }
+
+//   //   res.setHeader("Content-Type", "application/json");
 // });
 
-// app.get("/pets", (req, res) => {
-//   fs.readFile(petsPath, "utf8", function (err, petsJSON) {
-//     let pets = JSON.parse(petsJSON);
-//     if (err) {
-//       console.error(err.stack);
-//       res.statusCode = 500;
-//       res.setHeader("Content-Type", "text/plain");
-//       return res.end("Internal Server Error");
-//     }
-//     // res.writeHead(200, { "Content-Type": "application/json" });
-//     res.send(pets);
-//     //   res.setHeader("Content-Type", "application/json");
-//     res.end();
-//   });
+// server.listen(port, function () {
+//   console.log("Listening on port", port);
 // });
-
-// app.get("/pets/:petIndex", (req, res) => {
-//   fs.readFile(petsPath, "utf8", function (err, petsJSON) {
-//     let pets = JSON.parse(petsJSON);
-//     let index = pets[req.params.petIndex];
-
-//     if (err) {
-//       // console.error(err.stack);
-//       res.statusCode = 500;
-//       res.end();
-//     } else if (!index) {
-//       res.statusCode = 404;
-//       res.end("Not found");
-//     } else {
-//       res.send(index);
-//       res.status(200);
-//       res.end();
-//     }
-//   });
-// });
-
-// app.post("/pets", (req, res) => {
-//   console.log(req.body);
-//   console.log(req.body.age);
-//   let age = req.body.age;
-//   let kind = req.body.kind;
-//   let name = req.body.name;
-//   console.log(age);
-//   console.log(kind);
-//   console.log(name);
-//   // const { age, kind, name } = req.body;
-//   // console.log(age, kind, name);
-//   fs.readFile(petsPath, "utf8", (error, petsJSON) => {
-//     if (error) {
-//       res.statusCode = 500;
-//       res.end();
-//     }
-//     // let age = JSON.stringify(JSON.parse(req.body)).match("age");
-//     // let kind = JSON.stringify(JSON.parse(req.body)).match("kind");
-//     // let name = JSON.stringify(JSON.parse(req.body)).match("name");
-
-//     fs.readFile(petsPath, "utf8", (error, petsJSON) => {
-//       let pets = JSON.parse(petsJSON);
-//       let addPets = { age: age, kind: kind, name: name };
-//       pets.push(addPets);
-//       fs.writeFile(petsPath, JSON.stringify(pets), () => {
-//         console.log(addPets);
-//         res.end(petsJSON);
-//       });
-//     });
-//   });
-// });
-
-// app.listen(port, () => {
-//   console.log(`server listening on port: ${port}`);
-// });
-
-// // "use strict";
-
-// // var fs = require("fs");
-// // var path = require("path");
-// // var petsPath = path.join(__dirname, "pets.json");
-// // const express = require("express");
-
-// // var http = require("http");
-// // var port = process.env.PORT || 8000;
-
-// //
-// //   } else {
-// //     res.statusCode = 404;
-// //     res.setHeader("Content-Type", "text/plain");
-// //     res.end("Not found");
-// //   }
-// //   //   if (req.method === "POST" && req.url === "/pets") {
-// //   //     console.log("post");
-// //   //     if (err) {
-// //   //       console.error(err.stack);
-// //   //       res.statusCode = 500;
-// //   //       res.setHeader("Content-Type", "text/plain");
-// //   //       return res.end("Internal Server Error");
-// //   //     }else {
-// //   //         let age = process.argv[3];
-// //   //         let kind = process.argv[4];
-// //   //         let name = process.argv[5];
-// //   //         fs.readFile("pets.json", "utf8", (error, data) => {
-// //   //          let pets = JSON.parse(data);
-// //   //          let addPets = { age: age, kind: kind, name: name };
-// //   //         pets.push(addPets);
-// //   //         res.writeHead(200, { "Content-Type": "application/json" });
-// //   //           fs.writeFile("pets.json", JSON.stringify(pets), () => {
-// //   //             console.log(addPets);
-// //   //          res.end(petsJSON);
-// //   //          })
-// //   //         });
-// //   //      });
-// //   //     }
-
-// //   //   res.setHeader("Content-Type", "application/json");
-// // });
-
-// // server.listen(port, function () {
-// //   console.log("Listening on port", port);
-// // });
--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -64,66 +64,21 @@ app.post("/pets", (req, res) => {
   }
 });
 
-// app.post("/pets", (req, res) => {
-//   const { name, age, kind } = req.body;
-//   if (!name || !age || !kind || isNaN(age)) {
-//     res.status(400).send("Bad Request");
-//   } else {
-//     let newPet = {
-//       id: petsData.length + 1,
-//       age: parseInt(age),
-//       kind: kind,
-//       name: name,
-//     };
-//     petsData.push(newPet);
-//     res.status(200).json(newPet);
-//   }
-// });
+app.put("/pets/:id", (req, res) => {
+  let id = req.params.id;
+  let { name, age, kind } = req.body;
 
-// app.patch("/pets/:id", (req, res) => {
-//   const petId = req.params.id;
-//   const { name, age, kind } = req.body;
-
-//   // Check if pet exists
-//   client.query("SELECT * FROM pet WHERE id = $1", [petId]).then((result) => {
-//     if (result.rows.length === 0) {
-//       res.status(404).send("Pet not found");
-//     } else {
-//       const pet = result.rows[0];
-
-//       // Check for bad request
-//       if (
-//         (age && isNaN(age)) ||
-//         (kind && kind === "") ||
-//         (name && name === "")
-//       ) {
-//         res.status(400).send("Bad Request");
-//       } else {
-//         // Update the pet
-//         const updates = [];
-//         if (name) {
-//           pet.name = name;
-//           updates.push(`name='${name}'`);
-//         }
-//         if (age) {
-//           pet.age = parseInt(age);
-//           updates.push(`age=${parseInt(age)}`);
-//         }
-//         if (kind) {
-//           pet.kind = kind;
-//           updates.push(`kind='${kind}'`);
-//         }
-
-//         // Save changes to the database
-//         client
-//           .query(`UPDATE pet SET ${updates.join(",")} WHERE id=$1`, [petId])
-//           .then(() => {
-//             res.status(200).json(pet);
-//           });
-//       }
-//     }
-//   });
-// });
+  client.query(
+    "UPDATE pet SET name = $1, age = $2, kind = $3 WHERE id = $4",
+    [name, age, kind, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).send(`Pet modified with ID: ${id}`);
+    }
+  );
+});
 
 app.patch("/pets/:id", (req, res) => {
   let key = Object.keys(req.body)[0];
@@ -131,50 +86,24 @@ app.patch("/pets/:id", (req, res) => {
   client
     .query(`UPDATE pet SET ${key} = $1 WHERE id = $2 RETURNING *`, [
       value,
-      req.params.petId,
+      req.params.id,
     ])
     .then((result) => {
-      if (result.rows.length == 0) {
+      if (result.rows.length === 0) {
         res.status(404).send("pets not found");
       } else {
-        res.send(result.rows);
+        res.status(200).send(result.rows[0]);
       }
     });
 });
 
-// app.patch("/pets/:id", (req, res) => {
-//   const pet = petsData.find((pet) => pet.id === parseInt(req.params.id));
-//   if (!pet) {
-//     res.status(404).send("Not Found");
-//   } else {
-//     const { name, age, kind } = req.body;
-
-//     if ((age && isNaN(age)) || (kind && kind === "") || (name && name === "")) {
-//       res.status(400).send("Bad Request");
-//     } else {
-//       if (name) {
-//         pet.name = name;
-//       }
-//       if (age) {
-//         pet.age = parseInt(age);
-//       }
-//       if (kind) {
-//         pet.kind = kind;
-//       }
-//       res.status(200).json(pet);
-//     }
-//   }
-// });
 app.delete("/pets/:id", (req, res) => {
-  const index = petsData.findIndex((pet) => pet.id === parseInt(req.params.id));
-  if (index === -1) {
-    res.status(404).send("Not Found");
-  } else {
-    petsData.splice(index, 1);
-    res.status(200).json(pets);
-  }
+  client
+    .query("DELETE FROM pet WHERE id = $1", [req.params.id])
+    .then(() => res.send(`Pet with id ${req.params.id} deleted successfully`))
+    .catch((err) => res.status(500).send(err.message));
 });
 
 app.listen(port, () => {
-  console.log(`RestfulServer is listening on port:${port}`);
+  console.log(`restfulServer is listening on port:${port}`);
 });
